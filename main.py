7ff54b5f3b7516df5dbc8da26cdec51b802d53ca
gusu <spring.razer@gmail.com>
2024-10-03 20:34:41 +0800

    add

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, PlainTextResponse
 
-from config import LOGGER, WEWORK_CORPID, WEWORK_ENCODING_AES_KEY, WEWORK_TOKEN
-from util.wx_biz_json_msg_crypt import WXBizJsonMsgCrypt
+from config import LOGGER
+from wework import check_signature
 
 app = FastAPI()
+
 
 @app.get("/ping")
 async def ping():
     return {"message": "pong"}
 
+
 @app.get("/wechat/hook")
-async def wechat_hook_verification(msg_signature: str, timestamp: str, nonce: str, echostr: str):
-    msg_crypt = WXBizJsonMsgCrypt(WEWORK_TOKEN,
-                                  WEWORK_ENCODING_AES_KEY, 
-                                  WEWORK_CORPID)
-
-    ret, sEchoStr = msg_crypt.VerifyURL(msg_signature, timestamp, nonce, echostr)
-
-    LOGGER.info(f"VerifyURL ret: {ret}, sEchoStr: {sEchoStr}")
-    
+async def wechat_hook_verification(
+    msg_signature: str, timestamp: str, nonce: str, echostr: str
+):
+    ret, sEchoStr = check_signature(msg_signature, timestamp, nonce, echostr)
     if ret == 0:
         from fastapi.responses import PlainTextResponse
-        return PlainTextResponse(content=sEchoStr, media_type="text/plain;charset=utf-8")
+
+        return PlainTextResponse(
+            content=sEchoStr, media_type="text/plain;charset=utf-8"
+        )
     else:
         return JSONResponse(content={"error": "Verification failed"}, status_code=400)
+
 
 @app.post("/wechat/hook")
 async def wechat_hook_event(request: Request):
     # 处理企业微信服务器推送的事件
-    event_data = await request.json()
+    event_data = await request.body()
+    LOGGER.info("Received XML data", event_data)
     # 在这里处理事件数据
     return JSONResponse(content={"message": "Event received"})
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- wework.py
+++ wework.py
@@ -1,24 +1,42 @@
-# 
+#
 import time
-from config import WEWORK_CORPID, WEWORK_CORPSECRET, WEWORK_TOKEN_API
+from config import (
+    WEWORK_CORPID,
+    WEWORK_CORPSECRET,
+    WEWORK_ENCODING_AES_KEY,
+    WEWORK_TOKEN,
+    WEWORK_TOKEN_API,
+)
 import requests
+
+from util.wx_biz_json_msg_crypt import WXBizJsonMsgCrypt
 
 token_cache = {}
 
 
-def check_signature(signature, timestamp, nonce):
-    return True
+# 检查签名
+def check_signature(msg_signature, timestamp, nonce, echostr):
+    msg_crypt = WXBizJsonMsgCrypt(WEWORK_TOKEN, WEWORK_ENCODING_AES_KEY, WEWORK_CORPID)
+    ret, sEchoStr = msg_crypt.VerifyURL(msg_signature, timestamp, nonce, echostr)
+    return ret, sEchoStr
 
+
+# 可以缓存的token
 def cachable_token():
     current_time = time.time()
-    if not token_cache.get('token') or current_time - token_cache.get('timestamp', 0) > 3600:
-        token_cache['token'] = _wework_token()
-        token_cache['timestamp'] = current_time
-    return token_cache['token']
+    if (
+        not token_cache.get("token")
+        or current_time - token_cache.get("timestamp", 0) > 3600
+    ):
+        token_cache["token"] = _wework_token()
+        token_cache["timestamp"] = current_time
+    return token_cache["token"]
 
 
 def _wework_token():
-    response = requests.get(WEWORK_TOKEN_API, params={"corpid": WEWORK_CORPID, "corpsecret": WEWORK_CORPSECRET})
-    json_data =  response.json()
-    return json_data['AccessToken']
-
+    response = requests.get(
+        WEWORK_TOKEN_API,
+        params={"corpid": WEWORK_CORPID, "corpsecret": WEWORK_CORPSECRET},
+    )
+    json_data = response.json()
+    return json_data["AccessToken"]
