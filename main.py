8c178d8decb8d93094b0ec678f7615334a5be2a8
gusu <spring.razer@gmail.com>
2024-10-03 19:25:56 +0800

    fix

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ async def ping():
 async def wechat_hook_verification(msg_signature: str, timestamp: str, nonce: str, echostr: str):
     # 这里需要实现企业微信服务器验证逻辑
     # 验证成功后返回echostr
-    print(msg_signature, timestamp, nonce, echostr)
-
     msg_crypt = WXBizJsonMsgCrypt(WEWORK_TOKEN,
                                   WEWORK_ENCODING_AES_KEY, 
                                   WEWORK_CORPID)
